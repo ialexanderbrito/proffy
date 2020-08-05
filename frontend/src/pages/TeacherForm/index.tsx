46a54a6c17102c75c1418fdac5be4883d55e3f9b
Alexander <xanderzinho26@gmail.com>
2020-08-05 17:46:25 -0300

    Adiconado cadastro de horários

--- frontend/src/pages/TeacherForm/index.tsx
+++ frontend/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
@@ -10,6 +10,14 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import * as Styled from './styles';
 
 function TeacherForm() {
+  const [scheduleItems, setScheduleItems] = useState([
+    { week_day: 0, from: '', to: '' },
+  ]);
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 0, from: '', to: '' }]);
+  }
+
   return (
     <Styled.PageTeacherForm className="container">
       <PageHeader
@@ -57,25 +65,33 @@ function TeacherForm() {
         <Styled.Fieldset>
           <legend>
             Horários disponíveis
-            <button type="button">+ Novo horário</button>
+            <button type="button" onClick={addNewScheduleItem}>
+              + Novo horário
+            </button>
           </legend>
-          <Styled.ScheduleItem>
-            <Select
-              name="week_day"
-              label="Dia da Semana"
-              options={[
-                { value: '0', label: 'Domingo' },
-                { value: '1', label: 'Segunda-feira' },
-                { value: '2', label: 'Terça-feira' },
-                { value: '3', label: 'Quarta-feira' },
-                { value: '4', label: 'Quinta-feira' },
-                { value: '5', label: 'Sexta-feira' },
-                { value: '6', label: 'Sábado' },
-              ]}
-            />
-            <Input name="from" label="Das" type="time" />
-            <Input name="to" label="Até" type="time" />
-          </Styled.ScheduleItem>
+
+          {scheduleItems.map((scheduleItem) => {
+            return (
+              <Styled.ScheduleItem key={scheduleItem.week_day}>
+                <Select
+                  name="week_day"
+                  label="Dia da Semana"
+                  options={[
+                    { value: '0', label: 'Domingo' },
+                    { value: '1', label: 'Segunda-feira' },
+                    { value: '2', label: 'Terça-feira' },
+                    { value: '3', label: 'Quarta-feira' },
+                    { value: '4', label: 'Quinta-feira' },
+                    { value: '5', label: 'Sexta-feira' },
+                    { value: '6', label: 'Sábado' },
+                  ]}
+                />
+                <Input name="from" label="Das" type="time" />
+                <Input name="to" label="Até" type="time" />
+                <br />
+              </Styled.ScheduleItem>
+            );
+          })}
         </Styled.Fieldset>
 
         <Styled.Footer>
