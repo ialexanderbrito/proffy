a1290ef90577cf3f1be151a83dc84f1b2bdc2112
Alexander <xanderzinho26@gmail.com>
2020-08-05 20:36:19 -0300

    Adicionado total de conexão

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -8,9 +8,21 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import * as Styled from './styles';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <Styled.PageLanding>
       <Styled.PageLandingContent id="landing-content" className="container">
@@ -38,7 +50,7 @@ function Landing() {
         </Styled.ButtonsContainer>
 
         <Styled.TotalConnections>
-          Total de 200 conexões já realizadas{' '}
+          Total de {totalConnections} conexões já realizadas{' '}
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </Styled.TotalConnections>
       </Styled.PageLandingContent>
