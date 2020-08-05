10fba38dd18ff4c9cefb398065845cb0c625dcae
Alexander <xanderzinho26@gmail.com>
2020-08-05 14:35:48 -0300

    Adicionado estilização da descrição

--- frontend/src/components/PageHeader/styles.ts
+++ frontend/src/components/PageHeader/styles.ts
@@ -63,4 +63,12 @@ export const HeaderContent = styled.div`
     line-height: 4.2rem;
     color: var(--color-title-in-primary);
   }
+
+  p {
+    max-width: 30rem;
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+    color: var(--color-text-in-primary);
+    margin-top: 2.4rem;
+  }
 `;
