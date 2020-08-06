696297a89c2619a1a2ef509f243ec620a1fe2fc0
Alexander <xanderzinho26@gmail.com>
2020-08-05 21:24:51 -0300

    Adicionado informações puxadas da API

--- frontend/src/components/TeacherItem/index.tsx
+++ frontend/src/components/TeacherItem/index.tsx
@@ -4,33 +4,36 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import * as Styled from './styles';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+  subject: string;
+  cost: number;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <Styled.Article className="teacher-item">
       <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/36086408?s=460&u=b421d7781a879cfd7a33a04e783bccfb4afbc867&v=4"
-          alt="Alexander"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Alexander</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="WhatsApp" />
@@ -39,6 +42,6 @@ function TeacherItem() {
       </footer>
     </Styled.Article>
   );
-}
+};
 
 export default TeacherItem;
