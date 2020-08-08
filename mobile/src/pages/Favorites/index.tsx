f52f31f65d9ed8c0277fd336a0a799ddf625d666
Alexander <xanderzinho26@gmail.com>
2020-08-08 16:45:45 -0300

    Adicionado PageHeader nas pages

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import * as Styled from './styles';
 
 function Favorites() {
-  return <Styled.Container></Styled.Container>;
+  return (
+    <Styled.Container>
+      <PageHeader title="Meus proffys favoritos" />
+    </Styled.Container>
+  );
 }
 
 export default Favorites;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 import * as Styled from './styles';
 
 function TeacherList() {
-  return <Styled.Container></Styled.Container>;
+  return (
+    <Styled.Container>
+      <PageHeader title="Proffys disponíveis" />
+    </Styled.Container>
+  );
 }
 
 export default TeacherList;
