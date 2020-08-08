d8ed2255aa2cb84c76e8c86d7cb884774c298a1e
Alexander <xanderzinho26@gmail.com>
2020-08-08 15:11:09 -0300

    Estruturado e Estilizado page GiveClasses

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import * as Styled from './styles';
 
-function GiveClasses(){
-  return null;
+import bgImage from '../../assets/images/give-classes-background.png';
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <Styled.Container>
+      <Styled.Background resizeMode="contain" source={bgImage}>
+        <Styled.Title>Quer ser um Proffy?</Styled.Title>
+        <Styled.Description>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Styled.Description>
+      </Styled.Background>
+
+      <Styled.OkButton onPress={handleNavigateBack}>
+        <Styled.OkButtonText>Tudo bem</Styled.OkButtonText>
+      </Styled.OkButton>
+    </Styled.Container>
+  );
 }
 
 export default GiveClasses;
