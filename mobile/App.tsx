6616157396376b31845fdcf4e5cdf30ea2b45b5b
Alexander <xanderzinho26@gmail.com>
2020-08-08 14:50:03 -0300

    Adicionado AppStack

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing';
 import { AppLoading } from 'expo';
 import {
   Archivo_400Regular,
@@ -11,6 +10,8 @@ import {
   Poppins_400Regular,
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -26,7 +27,7 @@ export default function App() {
     return (
       <>
         <StatusBar style="light" />
-        <Landing />
+        <AppStack />
       </>
     );
   }
