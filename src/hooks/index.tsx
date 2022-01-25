b8d43117db1fba40aa8ccffcb734a458ea02fe2c
ialexanderbrito <xanderzinho26@gmail.com>
2022-01-25 17:28:17 -0300

    deploy

--- src/hooks/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { ThemeProvider } from './theme';
-
-const AppProvider: React.FC = ({ children }) => (
-  <ThemeProvider>{children}</ThemeProvider>
-);
-
-export default AppProvider;
